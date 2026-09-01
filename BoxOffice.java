a38c0c7c8fa14a3b678270f887d1508f6656edf6
cje361 <edwards.christian494@student.ccm.edu>
2026-09-01 14:37:36 -0400

    message

--- BoxOffice.java
+++ BoxOffice.java
@@ -1,4 +1,28 @@
+import java.util.Scanner;
+
 public class BoxOffice {
-    
+    public static void main(String[] args) {
+       Scanner input = new Scanner(System.in);
+       
+       System.out.println("Movie Name: ");
+       String MovieName =input.nextLine();
+
+       System.out.println("Adult tickets Sold: ");
+       int AdultSold =input.nextInt();
+
+       System.out.println("Child Tickets Sold: ");
+       int ChildSold =input.nextInt();
+
+       int AdultTicket = 10;
+       int ChildTicket = 6;
+       double interest = 0.2;
+       int AdultPrice = 0;
+       int ChildPrice = 0;
+
+       AdultTicket x AdultSold=AdultPrice;
+
+       System.out.println(AdultPrice);
+
+    }
     
 }
