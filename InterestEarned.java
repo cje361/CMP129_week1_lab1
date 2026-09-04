49030d537a511c482d219e6f747d5d74462b5ad5
cje361 <edwards.christian494@student.ccm.edu>
2026-09-04 14:23:49 -0400

    completed lab2

--- InterestEarned.java
+++ InterestEarned.java
@@ -24,8 +24,8 @@ public class InterestEarned {
         System.out.println("Interest Rate: " + interestrate + "%");
         System.out.println("Times Compounded: " + compound);
         System.out.println("Principal: $ " + principal);
-        System.out.printf("Interest: $ " + (amount - principal));
-        System.out.printf("\nAmount in Savings: $ " + amount);
+        System.out.printf("Interest: $ %,.2f",(amount - principal));
+        System.out.printf("\nAmount in Savings: $ %,.2f",amount);
 
     }
 }
